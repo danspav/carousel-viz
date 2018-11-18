ac3cf20c950fb62565bee8b897b0114e3b11cfd9
danspav <42852307+danspav@users.noreply.github.com>
2018-11-18 19:23:14 +1100

    Updates

--- appserver/static/visualizations/carousel-viz/src/visualization_source.js
+++ appserver/static/visualizations/carousel-viz/src/visualization_source.js
@@ -5,14 +5,20 @@ define([
             'jquery',
             'underscore',
             'api/SplunkVisualizationBase',
-            'api/SplunkVisualizationUtils'
+            'api/SplunkVisualizationUtils',
+			'carousel-viz',
+			'filltext',
+			'slick'
             // Add required assets to this list
         ],
         function(
             $,
             _,
             SplunkVisualizationBase,
-            vizUtils
+            vizUtils,
+			carousel_viz,
+			filltext,
+			slick
         ) {
   
     // Extend from SplunkVisualizationBase
@@ -21,10 +27,7 @@ define([
         initialize: function() {
             SplunkVisualizationBase.prototype.initialize.apply(this, arguments);
             this.$el = $(this.el);
-
-            this.$el.append('<h3>This is a custom visualization stand in.</h3>');
-            this.$el.append('<p>Edit your custom visualization app to render something here.</p>');
-            
+     
             // Initialization logic goes here
         },
 
@@ -32,9 +35,37 @@ define([
         // The returned object will be passed to updateView as 'data'
         formatData: function(data) {
 
-            // Format data 
+           // Check for an empty data object
+			if (data.rows.length < 1) {
+				return false;
+			}
+			// We need a minimum of 3 fields returned
+			if (data.fields.length < 3) {
+				throw new SplunkVisualizationBase.VisualizationError("Missing values. Please include the following fields in your search query: value, unit, caption");
+			}
 
-            return data;
+			//Make sure we have the following: value, unit, caption
+			var i = 0;
+			var hasValue = false;
+			var hasUnit = false;
+			var hasCaption = false;
+			var hasRange = false;
+			for (i = 0; i < data.fields.length; i++) {
+				if (data.fields[i].name == "value")
+					hasValue = true;
+				if (data.fields[i].name == "caption")
+					hasCaption = true;
+				if (data.fields[i].name == "unit")
+					hasUnit = true;
+				if (data.fields[i].name == "range" || data.fields[i].name == "style")
+					hasRange = true;
+			}
+
+			// Check for invalid data
+			if (!(hasValue && hasCaption && hasUnit )) {
+				throw new SplunkVisualizationBase.VisualizationError('Missing values. Please include the following fields in your search query: value, unit, caption. E.g. ...| table value, unit, caption');
+			}
+			return data;
         },
   
         // Implement updateView to render a visualization.
@@ -42,7 +73,27 @@ define([
         //  'config' will be the configuration property object
         updateView: function(data, config) {
             
-            // Draw something here
+            if (!data) {
+				return;
+			}
+
+			// Assign datum to the data object returned from formatData
+			if (!data.meta.done)
+				return;
+			
+			
+			// Clear the div
+			this.$el.empty();
+
+			var carousel_viz = require('carousel-viz');
+		
+			// Now load the visualisation
+			var oCarousel= new carousel_viz();
+			oCarousel.setConfig(config, this.getPropertyNamespaceInfo().propertyNamespace, mvc);
+			oCarousel.setData(data)
+			this.$el.html(oCarousel.getHTML());
+					
+			oCarousel.start();
 
         },
 
