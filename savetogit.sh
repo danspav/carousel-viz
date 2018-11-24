8e2f24ca591c5a79e88584afa4bd4e2c568215ae
danspav@gmail.com <danspav@nospam.com>
2018-11-25 10:45:46 +1100

    sadf

--- savetogit.sh
+++
@@ -1,6 +0,0 @@
-cp /opt/splunk/etc/apps/carousel-viz/local/data/ui/views/*.* /opt/git/carousel-viz/carousel-viz/default/data/ui/views/
-cp /opt/splunk/etc/apps/carousel-viz/local/data/ui/nav/*.* /opt/git/carousel-viz/carousel-viz/default/data/ui/nav/
-git add .
-git commit -m "Saved changes"
-git push https://danspav%40gmail.com:9fG!C@xPCAvqgM$5@7%D@github.com/danspav/carousel-viz.git
-
