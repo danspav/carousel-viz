f9767b92613626ba6b66f9cb62ebbdfd06c1c66c
danspav <42852307+danspav@users.noreply.github.com>
2019-01-03 10:53:39 +1100

    Update visualization_source.js

--- appserver/static/visualizations/carousel-viz/src/visualization_source.js
+++ appserver/static/visualizations/carousel-viz/src/visualization_source.js
@@ -76,8 +76,8 @@ define([
 			slick = require("slick");
 			this.oCarousel = oCarousel;
 			this.oCarousel.start();
-			this.$el.slickNext();
-			this.$el.slickPrevious();
+		//	this.$el.slickNext();
+		//	this.$el.slickPrevious();
 			//setTimeout(function(){oCarousel.resize(oCarousel.height );},1000);
 			//Set up the drilldown ability
 			var vizObj = this
